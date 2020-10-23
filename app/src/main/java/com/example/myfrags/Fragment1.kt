748bd1ff7c3c142172a3d99f85c840055842a4c2
jakubszczukin <jakub.szczukin@gmail.com>
2020-10-23 09:48:44 +0200

    Lab3 commit 23.10.2020

--- app/src/main/java/com/example/myfrags/Fragment1.kt
+++ app/src/main/java/com/example/myfrags/Fragment1.kt
@@ -1,10 +1,12 @@
 package com.example.myfrags
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +15,24 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Fragment1.newInstance] factory method to
+ * Use the [Fragment2.newInstance] factory method to
  * create an instance of this fragment.
  */
 class Fragment1 : Fragment() {
+
+    interface OnButtonClickListener {
+        fun onButtonClickShuffle()
+        fun onButtonClickClockwise()
+        fun onButtonClickHide()
+        fun onButtonClickRestore()
+    }
+
+    private var callback: OnButtonClickListener? = null
+
+    fun setOnButtonClickListener(callback: OnButtonClickListener?) {
+        this.callback = callback
+    }
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -33,8 +49,24 @@ class Fragment1 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val v: View = inflater.inflate(com.example.myfrags.R.layout.fragment_1, container, false)
+
+        val buttonShuffle: Button = v.findViewById(com.example.myfrags.R.id.button_shuffle) as Button
+        val buttonClockwise: Button = v.findViewById(com.example.myfrags.R.id.button_clockwise) as Button
+        val buttonHide: Button = v.findViewById(com.example.myfrags.R.id.button_hide) as Button
+        val buttonRestore: Button = v.findViewById(com.example.myfrags.R.id.button_restore) as Button
+
+        buttonShuffle.setOnClickListener { callback?.onButtonClickShuffle() }
+
+        buttonClockwise.setOnClickListener { callback?.onButtonClickClockwise() }
+
+        buttonHide.setOnClickListener { callback?.onButtonClickHide() }
+
+        buttonRestore.setOnClickListener { callback?.onButtonClickRestore() }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
+        return v
     }
 
     companion object {
@@ -44,12 +76,12 @@ class Fragment1 : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment1.
+         * @return A new instance of fragment Fragment2.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Fragment1().apply {
+            Fragment2().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/myfrags/MainActivity.kt
+++ app/src/main/java/com/example/myfrags/MainActivity.kt
@@ -1,26 +1,43 @@
 package com.example.myfrags
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import java.util.*
+import kotlin.collections.ArrayList
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity(), Fragment1.OnButtonClickListener {
+
+    private var frames: IntArray = intArrayOf()
+    private var hidden = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val fragment1 = Fragment1()
-        val fragment2 = Fragment2()
-        val fragment3 = Fragment3()
-        val fragment4 = Fragment4()
-
-        supportFragmentManager.inTransaction {add(R.id.frame1, fragment1)}
-        supportFragmentManager.inTransaction {add(R.id.frame2, fragment2)}
-        supportFragmentManager.inTransaction {add(R.id.frame3, fragment3)}
-        supportFragmentManager.inTransaction {add(R.id.frame4, fragment4)}
+        setContentView(com.example.myfrags.R.layout.activity_main)
+        if (savedInstanceState == null) {
+            frames = intArrayOf(
+                com.example.myfrags.R.id.frame1,
+                com.example.myfrags.R.id.frame2,
+                com.example.myfrags.R.id.frame3,
+                com.example.myfrags.R.id.frame4
+            )
+            hidden = false
+            val fragments: Array<Fragment> =
+                arrayOf(Fragment1(), Fragment2(), Fragment3(), Fragment4())
+            val fragmentManager = supportFragmentManager
+            val transaction = fragmentManager.beginTransaction()
+            for (i in 0..3) {
+                transaction.add(frames[i], fragments[i])
+            }
+            transaction.addToBackStack(null)
+            transaction.commit()
+        } else {
+            frames = savedInstanceState.getIntArray("FRAMES")!!
+            hidden = savedInstanceState.getBoolean("HIDEN")
+        }
     }
 
     inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
@@ -28,4 +45,71 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.func()
         fragmentTransaction.commit()
     }
+
+    override fun onButtonClickShuffle() {
+        val list: List<Int> = ArrayList<Int>(
+            Arrays.asList(
+                frames[0],
+                frames[1], frames[2], frames[3]
+            )
+        )
+        Collections.shuffle(list)
+        for (i in 0..3) frames[i] = list[i]
+        newFragments()
+    }
+
+    override fun onButtonClickClockwise() {
+        val t = frames[0]
+        frames[0] = frames[1]
+        frames[1] = frames[2]
+        frames[2] = frames[3]
+        frames[3] = t
+        newFragments()
+    }
+
+    override fun onButtonClickHide() {
+        if (hidden) return
+        val fragmentManager = supportFragmentManager
+        for (f in fragmentManager.fragments) {
+            if (f is Fragment1) continue
+            val transaction = fragmentManager.beginTransaction()
+            transaction.hide(f!!)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+        hidden = true
+    }
+
+    override fun onButtonClickRestore() {
+        if (!hidden) return
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        for (f in fragmentManager.fragments) {
+            if (f is Fragment1) continue
+            transaction.show(f!!)
+        }
+        transaction.addToBackStack(null)
+        transaction.commit()
+        hidden = false
+    }
+
+    override fun onAttachFragment(fragment: Fragment) {
+        super.onAttachFragment(fragment)
+        if (fragment is Fragment1) {
+            fragment.setOnButtonClickListener(this)
+        }
+    }
+
+    private fun newFragments() {
+        val newFragments = arrayOf(Fragment1(), Fragment2(), Fragment3(), Fragment4())
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        for (i in 0..3) {
+            transaction.replace(frames[i], newFragments[i])
+            if (hidden && newFragments[i] !is Fragment1) transaction.hide(newFragments[i])
+        }
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 }
